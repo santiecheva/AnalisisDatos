a670f0c848aec71e518e9340ebfefa89e5be6c3e
santiecheva <santiecheva@gmail.com>
2020-10-14 18:44:35 -0500

    prcentaje de datos nulos

--- prueba.py
+++ prueba.py
@@ -24,16 +24,16 @@ def sql_table(con):
 
     df_train =  pd.read_sql_query("SELECT * from train", con)
     df_test =  pd.read_sql_query("SELECT * from test", con)
-    #df_ix_train_index =  pd.read_sql_query("SELECT * from ix_train_index", con)
-    #df_ix_test_index =  pd.read_sql_query("SELECT * from ix_test_index", con)
+
 
 
     con.commit()
 
-    print('de la tabla train: \n %s' % df_train.isnull().sum())   
-    print('de la tabla train: \n %s' % df_train.isnull().sum())   
+    print('de la tabla train se tienen nulos: \n %s' % df_train.isnull().sum())   
+    print('de la tabla test se tienen nulos: \n %s' % df_test.isnull().sum())   
 
-
+    print('de la tabla train se tiene el porcentaje de nulos: \n ' , df_train.isnull().sum()/len(df_train)*100)   
+    print('de la tabla test se tiene el porcentaje de nulos: \n ' , df_test.isnull().sum()/len(df_test)*100)  
 
 con = sql_connection()
 
